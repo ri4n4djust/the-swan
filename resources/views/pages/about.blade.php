482815c9d04f13f8c8f24556d73f2cb699b58914
ri4n4djust <rian.adjust@gmail.com>
2024-03-17 12:45:11 +0800

    update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -8,37 +8,45 @@
         <div class="container" data-aos="fade-up">
 
           <div class="section-header">
-            <!-- <h2>About Us</h2> -->
-            <p>Learn More <span>About Us</span></p>
+            <h2>{{ __('headmenu.about_us') }}</h2>
+            <p>{{ __('headmenu.about_des') }} <span></span></p>
           </div>
 
           <div class="row gy-4">
             <div class="col-lg-7 position-relative about-img" style="background-image: url(assets/img/about.jpg) ;" data-aos="fade-up" data-aos-delay="150">
               <div class="call-us position-absolute">
-                <h4>Book a Table</h4>
-                <p>+1 5589 55488 55</p>
+                <h4>{{ __('headmenu.about_book') }}</h4>
+                <p>
+                  <a href="https://api.whatsapp.com/send?phone=+6282340064488&text=Halo" target="_blank" >
+                    <button style="vertical-align:center;border-radius:5px">
+                    <img src="assets/img/wa.png" class="img-fluid">+62 8234 006 4488</button>
+                    
+                  </a>
+                </p>
+                
               </div>
             </div>
             <div class="col-lg-5 d-flex align-items-end" data-aos="fade-up" data-aos-delay="300">
               <div class="content ps-0 ps-lg-5">
                 <p class="fst-italic">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                  magna aliqua.
+                  We are company that manage property and also rent for acomodation. 
                 </p>
                 <ul>
-                  <li><i class="bi bi-check2-all"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                  <li><i class="bi bi-check2-all"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                  <li><i class="bi bi-check2-all"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                  <li><i class="bi bi-check2-all"></i> As traveller : we are your friend, family and guide as you need.</li>
+                  <li><i class="bi bi-check2-all"></i> As Traveller : we make your holly day to be awesome and memoriable,.</li>
+                  <li><i class="bi bi-check2-all"></i> As Traveller : rooms, transport, private tour as you wish or our idea.</li>
                 </ul>
-                <p>
-                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-                </p>
-
-                <div class="position-relative mt-4">
+                <p>dont be hastitate to contact us on mobile or whatsapp number 24H/27.</p>
+                <p>Or you have property still confuse to manage, don't worry sit back, take a braath and relax let our team do the best.</p>
+                <ul>
+                  <li><i class="bi bi-check2-all"></i> As Property Owner : Increase booking from OTA.</li>
+                  <li><i class="bi bi-check2-all"></i> As Property Owner : Increase Revenue, neat report, </li>
+                  <li><i class="bi bi-check2-all"></i> As Property Owner : and also taking care all about the property.</li>
+                </ul>
+                <!-- <div class="position-relative mt-4">
                   <img src="assets/img/about-2.jpg" class="img-fluid" alt="">
                   <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox play-btn"></a>
-                </div>
+                </div> -->
               </div>
             </div>
           </div>
