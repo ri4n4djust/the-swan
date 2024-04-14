6937628cea67294bbb9105b220ef0a1339092982
ri4n4djust <rian.adjust@gmail.com>
2024-04-14 09:22:12 +0800

    Update default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,6 +7,16 @@
 @yield('media')
 </head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YGQ91MNY00"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YGQ91MNY00');
+</script>
+
 <body>
 
   <!-- ======= Header ======= -->
