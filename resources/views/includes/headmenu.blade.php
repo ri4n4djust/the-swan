9ca0fde0402fba866c5db7d3fea40a3944968d50
ri4n4djust <rian.adjust@gmail.com>
2024-04-17 09:10:39 +0800

    Update headmenu.blade.php

--- resources/views/includes/headmenu.blade.php
+++ resources/views/includes/headmenu.blade.php
@@ -29,28 +29,20 @@
         ?> 
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="/destinations">{{ __('headmenu.destinations') }}</a></li>
-          <li><a href="/activities">{{ __('headmenu.activity') }}</a></li>
+          <li class="dropdown"><a href="#"><span>Our Service</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="/hotels">Rooms Booking</a></li>
+              <li><a href="/destinations">{{ __('headmenu.destinations') }}</a></li>
+              <li><a href="/activities">{{ __('headmenu.activity') }}</a></li>
+              <!-- <li><a href="#">Drop Down 4</a></li> -->
+            </ul>
+          </li>
+          <!-- <li><a href="/destinations">{{ __('headmenu.destinations') }}</a></li> -->
+          <!-- <li><a href="/activities">{{ __('headmenu.activity') }}</a></li> -->
           <!-- <li><a href="/events">{{ __('headmenu.event') }}</a></li> -->
           <li><a href="/partnership">{{ __('headmenu.partnership') }}</a></li>
           <li><a href="/gallery">{{ __('headmenu.galleri') }}</a></li>
-          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li> -->
+          
           <li><a href="/contact">{{ __('headmenu.contact') }}</a></li>
           <li class="dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
