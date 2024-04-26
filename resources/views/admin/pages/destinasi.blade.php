8c8178870f4184dbe134ed42ad4040db342b2034
ri4nb4gus <rian.adjust@gmail.com>
2024-04-26 10:28:57 +0800

    update

--- resources/views/admin/pages/destinasi.blade.php
+++ resources/views/admin/pages/destinasi.blade.php
@@ -10,6 +10,17 @@
       </div>
       <div class="card-body">
         <div class="table-responsive">
+
+        <div class="row">
+          <div class="form-group col-lg-3">
+              <input type="text" name="code" class="form-control" placeholder="Judul" >
+          </div>
+          <div class="form-group col-lg-2">
+           
+            <button type="submit" class="btn btn-fill btn-primary">Cari</button>
+          </div>
+        </div>
+
           <table class="table tablesorter " id="">
             <thead class=" text-primary">
               <tr>
